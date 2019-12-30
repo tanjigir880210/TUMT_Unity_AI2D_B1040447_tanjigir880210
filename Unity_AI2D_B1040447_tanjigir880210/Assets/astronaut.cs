3cc33b707a541229f341a77b7963dcfedf5b4041
tanjigir880210 <55218583+tanjigir880210@users.noreply.github.com>
2019-12-31 07:26:12 +0800

    1081231

---
+++ Unity_AI2D_B1040447_tanjigir880210/Assets/astronaut.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class astronaut : MonoBehaviour
+{
+    public int speed = 50;
+    public float jump = 2.5f;
+    public string astronautName = "太空人";
+    public bool pass = false;
+
+    private void Start()
+    {
+        Debug.Log("呼叫太空人");
+    }
+}
