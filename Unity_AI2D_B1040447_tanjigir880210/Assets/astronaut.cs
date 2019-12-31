3ac64a9cd0cdbc8e191204ad55f963667c792724
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2019-12-31 11:36:14 +0800

    1231_11點

--- Unity_AI2D_B1040447_tanjigir880210/Assets/astronaut.cs
+++ Unity_AI2D_B1040447_tanjigir880210/Assets/astronaut.cs
@@ -9,13 +9,23 @@ public class astronaut : MonoBehaviour
     public bool pass = false;
 
     private Rigidbody2D r2d;
+    // private Transform tra;
     private void Start()
     {
         r2d = GetComponent<Rigidbody2D>();
+        // tra = GetComponent<Transform>();
     }
+
     private void Update()
     {
-        Debug.Log(Input.GetAxis("Horizontal"));
+        if (Input.GetKeyDown(KeyCode.A)) transform.eulerAngles = new Vector3(0, 0, 0);
+        if (Input.GetKeyDown(KeyCode.D)) transform.eulerAngles = new Vector3(0, 180, 0);
+        if (Input.GetKeyDown(KeyCode.RightArrow)) transform.eulerAngles = new Vector3(0, 180, 0);
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) transform.eulerAngles = new Vector3(0, 0, 0);
+    }
+    private void FixedUpdate()
+    {
+        // Debug.Log(Input.GetAxis("Horizontal"));
         r2d.AddForce(new Vector2(speed * Input.GetAxis("Horizontal"), 0));
     }
 }
