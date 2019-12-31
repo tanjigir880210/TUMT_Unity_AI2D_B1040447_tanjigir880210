cd416508283622315db9594cf64a14a50ae4d09a
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2019-12-31 12:34:37 +0800

    能夠跳躍

--- Unity_AI2D_B1040447_tanjigir880210/Assets/astronaut.cs
+++ Unity_AI2D_B1040447_tanjigir880210/Assets/astronaut.cs
@@ -9,23 +9,46 @@ public class astronaut : MonoBehaviour
     public bool pass = false;
 
     private Rigidbody2D r2d;
-    // private Transform tra;
     private void Start()
     {
         r2d = GetComponent<Rigidbody2D>();
-        // tra = GetComponent<Transform>();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A)) transform.eulerAngles = new Vector3(0, 0, 0);
-        if (Input.GetKeyDown(KeyCode.D)) transform.eulerAngles = new Vector3(0, 180, 0);
-        if (Input.GetKeyDown(KeyCode.RightArrow)) transform.eulerAngles = new Vector3(0, 180, 0);
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) transform.eulerAngles = new Vector3(0, 0, 0);
+        if (Input.GetKeyDown(KeyCode.A)) Turn(180);
+        if (Input.GetKeyDown(KeyCode.D)) Turn(0);
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) Turn(180);
+        if (Input.GetKeyDown(KeyCode.RightArrow)) Turn(0);
     }
     private void FixedUpdate()
     {
-        // Debug.Log(Input.GetAxis("Horizontal"));
+        Walk();
+        Jump();
+    }
+
+    /// <summary>
+    /// 走路
+    /// </summary>
+    private void Walk()
+    {
         r2d.AddForce(new Vector2(speed * Input.GetAxis("Horizontal"), 0));
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+        r2d.AddForce(new Vector2(0, jump * Input.GetAxis("Jump")));
+    }
+
+    /// <summary>
+    /// 轉彎
+    /// </summary>
+    /// <param name="direction">方向，左轉為 180，右轉為 0</param>>
+    private void Turn(int direction = 0)
+    {
+        transform.eulerAngles = new Vector3(0, direction, 0);
     }
 }
