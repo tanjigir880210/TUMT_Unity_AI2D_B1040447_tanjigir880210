60c3fbd34c8423c64f85e7fcb8bfae977f2d5caf
tanjigir880210 <55218583+tanjigir880210@users.noreply.github.com>
2020-01-03 12:44:15 +0800

    0103

---
+++ Unity_AI2D_B1040447_tanjigir880210/Assets/scripts/enemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    [Header("移動速動"), Range(0, 100)]
+    public float speed = 1.5f;
+    [Header("傷害"), Range(0, 100)]
+    public float damage = 20;
+
+    public Transform checkPoint;
+
+    private Rigidbody2D r2d;
+
+    private void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(checkPoint.position, -checkPoint.up * 4);
+    }
+
+    /// <summary>
+    /// 隨機移動
+    /// </summary>
+    private void Move()
+    {
+        r2d.AddForce(new Vector2(-speed, 0));
+    }
+
+    /// <summary>
+    /// 追蹤
+    /// </summary>
+    private void Track()
+    {
+
+    }
+}
